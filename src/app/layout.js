d6179a14d1bec1307ced5e51f3085350a37393dd
hanaisreal <ohhanathefirst@gmail.com>
2024-02-11 21:04:22 +0900

    added login page and authentification

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Roboto} from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
+import { AppProvider } from "../components/AppContext";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ['400', '500','700']});
 require('dotenv').config({ path: './src/.env' });
@@ -15,13 +16,15 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={roboto.className}>
         <main className="max-w-4xl mx-auto border p-4">
-          <Header />
-          {children}
-          <footer className="border-t p-8 text-center text-gray-500 my-16">
-        &copy; 2024 All rights reserved
-        </footer>
+          <AppProvider>
+            <Header />
+            {children}
+            <footer className="border-t p-8 text-center text-gray-500 my-16">
+              &copy; 2024 All rights reserved
+            </footer>
+          </AppProvider>
         </main>
-        </body>
+      </body>
     </html>
   );
 }
---
+++ src/app/login/page.js
@@ -0,0 +1,43 @@
+'use client';
+import {signIn} from "next-auth/react";
+import Image from "next/image";
+import {useState} from "react";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginInProgress, setLoginInProgress] = useState(false);
+
+  async function handleFormSubmit(ev) {
+    ev.preventDefault();
+    setLoginInProgress(true);
+
+    await signIn('credentials', {email, password, callbackUrl: '/'}); 
+
+    setLoginInProgress(false);
+  }
+  return (
+    <section className="mt-8">
+      <h1 className="text-center text-primary text-4xl mb-4">
+        로그인
+      </h1>
+      <form className="max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+        <input type="email" name="email" placeholder="이메일" value={email}
+               disabled={loginInProgress}
+               onChange={ev => setEmail(ev.target.value)} />
+        <input type="password" name="password" placeholder="비밀번호" value={password}
+               disabled={loginInProgress}
+               onChange={ev => setPassword(ev.target.value)}/>
+        <button disabled={loginInProgress} type="submit">로그인</button>
+        <div className="my-4 text-center text-gray-500">
+        또는 간편하게 연결하기
+        </div>
+        <button type="button" onClick={() => signIn('google', {callbackUrl: '/'})}
+                className="flex gap-4 justify-center">
+          <Image src={'/google.png'} alt={''} width={24} height={24} />
+          Login with google
+        </button>
+      </form>
+    </section>
+  );
+}
