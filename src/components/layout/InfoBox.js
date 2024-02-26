b2706e66a5289b1e3d5ede83b65961bfaae1517c
hanaisreal <ohhanathefirst@gmail.com>
2024-02-26 13:37:50 +0900

    refractor: other method than react-hot-toast(not longer used)

---
+++ src/components/layout/InfoBox.js
@@ -0,0 +1,5 @@
+export default function InfoBox({children}){
+    <div className="text-center text-primary text-2xl mb-4">
+        {children}
+    </div>
+}
---
+++ src/components/layout/SuccessBox.js
@@ -0,0 +1,6 @@
+export default function SuccessBox({children}) {
+    <div className="text-center bg-green-100 p-4 rounded-lg border
+    border-green-300"
+    > {children}</div>
+
+}
